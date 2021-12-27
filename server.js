da1a43c237963476f5a149228d5d8c67f892f687
unknown <adjurdjina@gmail.com>
2021-12-27 12:01:05 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData=[
 {name:'Mark'},
 {name:'Jill'}
@@ -13,9 +16,27 @@ app.get('/users', function(req,res){
  	})
 })
 
-
-
-
+// colons are used as variables that be viewed in the params
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+ 
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+ 
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
+})
 
 
 app.listen(8000,function(){
